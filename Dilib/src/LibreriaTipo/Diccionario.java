d6dc8496b1f9ec5a15f53a199d3c894aa38f1f4f
Estudiante <Estudiante@lab0508>
2018-01-10 08:48:15 -0500

    echa la clase diccionario

--- Dilib/src/LibreriaTipo/Diccionario.java
+++ Dilib/src/LibreriaTipo/Diccionario.java
@@ -5,10 +5,54 @@
  */
 package LibreriaTipo;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  *
  * @author Alex Reinoso
  */
-public class Diccionario {
+public class Diccionario 
+{
+    private HashMap<Integer,Persona> elemento;
+    private List<Persona> lista;
+
+    public boolean agregaElemento(int K, Persona E)
+    {
+        boolean band=false;
+        elemento.put(K, E);
+        band=true;
+        return band;
+    }
+    
+    public Object recuperarElemento(int K)
+    {
+        Object k=elemento.get(K);
+        
+        return  k;
+    }
+    
+    public List<Persona> recuperarElementos()
+    {
+        Iterator it = elemento.keySet().iterator();
+        
+        while(it.hasNext())
+        {
+            Integer key = (Integer) it.next();
+            lista.add(elemento.get(key));
+        }
+        
+        return lista;
+    }
+    
+    public boolean eliminarElemento(int K)
+    {
+        boolean band=false;
+        elemento.remove(K);
+        band=true;
+        
+        return band;
+    }
     
 }
